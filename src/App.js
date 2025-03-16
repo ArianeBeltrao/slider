6cf27934d86d8500b00f21c35bc8a348cc4f1bde
Ariane <ariane@globalfy.com>
2025-03-16 17:27:13 -0300

    refactor: improved layers and deleted unused code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 
 import './App.css';
-import Slider from './Slider.js'
+import Slider from './components/Slider.js'
+import { ArrayImages } from './data/ArrayImages.js';
 
-function App() {
-
-  const arrayImages = [
-    {
-      id: '1',
-      url: 'https://plus.unsplash.com/premium_photo-1721178570714-b6917abf6754?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Imagem de um buraco negro no ceu',
-      link: 'https://control.xlow.com/'
-    },
-    {
-      id: '2',
-      url: 'https://plus.unsplash.com/premium_photo-1740997621945-f01bb06527df?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Ilustracao do reator nuclear cercado por atomo',
-      link: 'https://control.xlow.com/'
-    },
-    {
-      id: '3',
-      url: 'https://images.unsplash.com/photo-1741017162002-fac8e37509f9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'A nebulosa da roseta uma rosa cosmica no espaco',
-      link: 'https://control.xlow.com/'
-    },
-    {
-      id: '4',
-      url: 'https://images.unsplash.com/photo-1741515277598-64b4da5d212a?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Vista aerea de uma casa cercada por arvores cor de rosa',
-      link: 'https://control.xlow.com/'
-    },
-    {
-      id: '5',
-      url: 'https://plus.unsplash.com/premium_photo-1701105794582-3d15d822f223?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Um grupo de arvores que estao no escuro',
-      link: 'https://control.xlow.com/'
-    }
-  ]
+export default function App() {
   return (
     <div className="App">
       <h2>SLIDER PAGE</h2>
-      <Slider images={arrayImages}/>
+      <Slider images={ArrayImages}/>
     </div>
   );
 }
-
-export default App;
